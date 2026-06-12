8c9a48846827befbea3d95685fd7b71c2c93498b
jeffrinjames99 <jeffrinjames99@gmail.com>
2026-06-12 05:50:19 +0000

    Fix Room build: fix entity imports and remove undefined NSFWEvent/NSFWEventDao

--- android/app/src/main/java/com/manwen/app/data/local/AppDatabase.kt
+++ android/app/src/main/java/com/manwen/app/data/local/AppDatabase.kt
@@ -2,13 +2,16 @@ package com.manwen.app.data.local
 
 import android.content.Context
 import androidx.room.*
-import com.manwen.app.data.local.entities.AppDatabaseEntities
+import com.manwen.app.data.local.entities.UserProgress
+import com.manwen.app.data.local.entities.DailyCheckIn
+import com.manwen.app.data.local.entities.UrgeSurfingSession
+import com.manwen.app.data.local.entities.ManagedApp
+import com.manwen.app.data.local.entities.LocalAnalyticsEvent
 
 @Database(
     entities = [
         UserProgress::class,
         DailyCheckIn::class,
-        NSFWEvent::class,
         UrgeSurfingSession::class,
         ManagedApp::class,
         LocalAnalyticsEvent::class,
@@ -19,7 +22,6 @@ import com.manwen.app.data.local.entities.AppDatabaseEntities
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userProgressDao(): com.manwen.app.data.local.daos.UserProgressDao
     abstract fun dailyCheckInDao(): com.manwen.app.data.local.daos.DailyCheckInDao
-    abstract fun nsfwEventDao(): com.manwen.app.data.local.daos.NSFWEventDao
     abstract fun urgeSurfingDao(): com.manwen.app.data.local.daos.UrgeSurfingDao
     abstract fun managedAppsDao(): com.manwen.app.data.local.daos.ManagedAppsDao
     abstract fun analyticsDao(): com.manwen.app.data.local.daos.AnalyticsDao
