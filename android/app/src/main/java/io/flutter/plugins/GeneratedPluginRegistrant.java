8ba9d0671b585a3703f438978011b700fbdbbdcc
Unselfisheologism <jeffrinjames99@gmail.com>
2026-06-12 18:44:27 +0530

    remove unused path_provider, run flutter pub get in root build.gradle, add empty GeneratedPluginRegistrant

---
+++ android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -0,0 +1,12 @@
+package io.flutter.plugins;
+
+import io.flutter.embedding.engine.FlutterEngine;
+
+/**
+ * Manually provided GeneratedPluginRegistrant that registers no plugins (working around
+ * missing .flutter-plugins-dependencies in bare Gradle builds).
+ */
+public final class GeneratedPluginRegistrant {
+    public static void registerWith(FlutterEngine engine) {
+    }
+}
